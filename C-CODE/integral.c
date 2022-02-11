40756e61e8ba23464e1adadf9723f88834695fdd
Max Rabe <rabe.maximilian@gmail.com>
2022-02-11 08:50:03 +0100

    Simplify trial cloning

--- C-CODE/integral.c
+++ C-CODE/integral.c
@@ -143,17 +143,12 @@ swift_run * new_swift_trial(swift_model * model, int s, unsigned int seed) {
 
 swift_run * clone_swift_trial(swift_run * src, unsigned int seed) {
 	swift_run * ret = malloc(sizeof(swift_run));
+	*ret = *src;
 	ret->n_count = duplicate_vector(int, src->n_count, src->N + 4);
 	ret->prev_n_count = duplicate_vector(int, src->prev_n_count, src->N + 4);
 	ret->N_count = duplicate_vector(int, src->N_count, src->N + 4);
 	ret->states = duplicate_vector(int, src->states, src->N + 4);
 	ret->W = duplicate_vector(double, src->W, src->N + 4);
-	ret->N = src->N;
-	ret->t = src->t;
-	ret->dt = src->dt;
-	ret->canc = src->canc;
-	ret->is_mislocated = src->is_mislocated;
-	ret->is_refixation = src->is_refixation;
 	ret->ptar = duplicate_vector(double, src->ptar, src->N);
 	ret->aa = duplicate_vector(double, src->aa, src->N);
 	ret->view = duplicate_vector(double, src->view, src->N);
@@ -161,16 +156,6 @@ swift_run * clone_swift_trial(swift_run * src, unsigned int seed) {
 	ret->len = duplicate_vector(int, src->len, src->N);
 	ret->aa = duplicate_vector(double, src->aa, src->N);
 	ret->procrate = duplicate_vector(double, src->procrate, src->N);
-	ret->gaze_word = src->gaze_word;
-	ret->gaze_letter = src->gaze_letter;
-	ret->corpus = src->corpus;
-	ret->params = src->params;
-	ret->s = src->s;
-	ret->saccade_target = src->saccade_target;
-	ret->dist = src->dist;
-
-	initSeed(seed ? seed : ranint(&src->seed), &ret->seed);
-
 	return ret;
 }
 
